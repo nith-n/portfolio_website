5aa3aff31a095b682420ad2e38babfca9a964a3b
Nithin Nedumaparambil <nt923837@dal.ca>
2025-03-21 19:17:21 -0300

    Modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.html'],
-  server: {
-    port: 5173,
-    strictPort: false,
-    host: true
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
-  }
+  },
+  base: '/', 
 })
